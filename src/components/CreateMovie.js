6e5026f927591fa0b0067722f2be34ea09496165
kuhuhalder <kuhuhalder2701@gmail.com>
2023-02-25 18:09:58 -0500

    updates to template

---
+++ src/components/CreateMovie.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const CreateMovie = (props) => {
+  const navigate = useNavigate();
+  const [movie, setMovie] = useState({
+    title: "",
+    review: "",
+  });
+
+  const onChange = (e) => {
+   
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+   
+  };
+
+  return (
+    <div className="CreateMovie">
+      <div className="container">
+        <div className="row">
+          <div>
+            <br />
+            <Link to="/">
+              Show Movie List
+            </Link>
+          </div>
+          <div>
+            <h1>Add Movie</h1>
+            <p>Create new Movie</p>
+
+            <form noValidate onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Title of the Movie"
+                  name="title"
+                  className="form-control"
+                  value={movie.title}
+                  onChange={onChange}
+                />
+              </div>
+              <br />
+
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Review"
+                  name="review"
+                  className="form-control"
+                  value={movie.review}
+                  onChange={onChange}
+                />
+              </div>
+
+              <input
+                type="submit"
+                className="btn btn-outline-warning btn-block mt-4"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateMovie;
